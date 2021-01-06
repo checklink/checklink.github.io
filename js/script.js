a35fc01fc0bcfcd3f7c37e19e20eec3f4426edb9
bra1nf00d <pixeljack3@gmail.com>
2021-01-06 22:22:18 +0200

    ddd

--- js/script.js
+++ js/script.js
@@ -1,31 +1,28 @@
-let menu = document.querySelector('.icon-menu');
-let list = document.querySelector('.menu__list');
-let li = list.querySelectorAll('.menu__link');
+localStorage.a;
+let $btn = document.getElementById('btn');
+let $counter = document.querySelector('.block__counter');
+let num = localStorage.a || 0;
+$counter.textContent = num;
 
-for (let i = 0; i < li.length; i++) {
-	li[i].textContent += ".";
-};
+// add
+$btn.onclick = () => {
+	num++;
+	save()
+	if (num == 12) alert('you are super');
+}
 
-menu.addEventListener('click', () => {
-	let menuBody = document.querySelector('.menu__body');
-	let pageBody = document.querySelectorAll('.fp-tableCell');
-	let nav = document.querySelector('#fp-nav');
-	menuBody.classList.toggle('active');
-	menu.classList.toggle('focus');
-	nav.classList.toggle('hide');
-	for (let i = 0; i < li.length; i++) {
-		li[i].addEventListener('click', () => {
-			menuBody.classList.remove('active');
-			menu.classList.remove('focus');
-			nav.classList.remove('hide');
-		});
-		pageBody[i].addEventListener('click', () => {
-			menuBody.classList.remove('active');
-			menu.classList.remove('focus');
-			nav.classList.remove('hide');
-		});
-	};
-});
-let totalSlide = document.querySelector('.slider-counter__total');
-let totalSection = document.querySelectorAll('.section');
-totalSlide.textContent += totalSection.length;
+// delete
+let $btn2 = document.getElementById('btn2');
+$btn2.onclick = () => {
+	num = 0;
+	save()
+}
+
+// function
+function save() {
+	$counter.textContent = num;
+	localStorage.a = num;
+	console.log(num);
+}
+
+
